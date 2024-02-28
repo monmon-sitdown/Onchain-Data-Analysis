2c893540b80bdd7daf69b51c3d281b2f23f1e853
monmon <syu24059@gmail.com>
2024-02-28 15:06:44 +0900

    Update

--- 2_UniswapMultichain/04_QuerySingleChain.sql
+++ 2_UniswapMultichain/04_QuerySingleChain.sql
@@ -2,6 +2,6 @@ select block_date,
     active_user_count,
     new_user_count,
     retain_user_count
-from query_3412141 -- This points to all returned data from query https://dune.com/queries/1928825
+from query_3412141 -- This points to all returned data from query https://dune.com/queries/3412141
 where blockchain = 'etherum'
 order by block_date
